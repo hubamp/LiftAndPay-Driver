77e1341346f78f394679f8db84774554b466bb27
hubamp <hubamp@gmail.com>
2021-01-20 12:02:54 +0000

    Outline interfaces for the four uploading processes has been done. Transition animation from one activity to the other has been completed.

---
+++ app/src/main/java/com/example/liftandpay_driver/UploadDetailsActivity_1.java
@@ -0,0 +1,44 @@
+package com.example.liftandpay_driver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class UploadDetailsActivity_1 extends AppCompatActivity {
+  private View headerView;
+  private View footerView;
+  private LinearLayout headerLayout;
+  private LinearLayout footerLayout;
+  private ImageButton proceedImgBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_details_1);
+
+        headerLayout = findViewById(R.id.headerView);
+        footerLayout = findViewById(R.id.footerView);
+
+        headerView = getLayoutInflater().inflate(R.layout.header_view, null);
+        footerView = getLayoutInflater().inflate(R.layout.footer_view, null);
+        headerLayout.addView(headerView);
+        footerLayout.addView(footerView);
+
+        proceedImgBtn = findViewById(R.id.btn_proceed_id);
+
+
+        proceedImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UploadDetailsActivity_1.this , UploadDetailsActivity_2.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/liftandpay_driver/UploadDetailsActivity_2.java
@@ -0,0 +1,43 @@
+package com.example.liftandpay_driver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+public class UploadDetailsActivity_2 extends AppCompatActivity {
+
+    private View headerView;
+    private View footerView;
+    private LinearLayout headerLayout;
+    private LinearLayout footerLayout;
+    private ImageButton proceedImgBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_details_2);
+
+        headerLayout = findViewById(R.id.headerView);
+        footerLayout = findViewById(R.id.footerView);
+
+        headerView = getLayoutInflater().inflate(R.layout.header_view, null);
+        footerView = getLayoutInflater().inflate(R.layout.footer_view, null);
+        headerLayout.addView(headerView);
+        footerLayout.addView(footerView);
+
+        proceedImgBtn = findViewById(R.id.btn_proceed_id);
+
+
+        proceedImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UploadDetailsActivity_2.this , null));
+
+            }
+        });
+    }
+}
